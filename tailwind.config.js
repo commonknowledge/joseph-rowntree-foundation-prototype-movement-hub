81d7a5adbce21ad189934b3e132e77ab3a655505
Jan Baykara <janbaykara@pm.me>
2021-05-25 12:58:38 +0100

    Add in JRF colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {}
+      colors: {
+        text: "#222222",
+        border: "#2F3130",
+        green: "#BEC4A0",
+        greenLight: "#D6DAC6",
+        yellow: "#F5D800",
+        yellowLight: "#F7F1C1",
+        red: "#C94E3E",
+        redLight: "#F2B7B1",
+        blue: "#2C2264",
+        blueLight: "#DAD6F0",
+      }
     },
   },
   variants: {},
