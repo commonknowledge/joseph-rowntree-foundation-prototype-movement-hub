877f1dee87043b6f13da8d1b1c2e34e15872a61f
Jan Baykara <janbaykara@pm.me>
2021-06-03 00:02:20 +0100

    Add links to slack, newsletter and booking service

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.get(
+  '/join-the-community', (req, res) =>
+  res.redirect(process.env.SLACK_INVITE_URL)
+)
+router.get(
+  '/newsletter-subscription', (req, res) =>
+  res.redirect('https://www.jrf.org.uk/subscription')
+)
+router.get(
+  '/book-a-meeting', (req, res) =>
+  res.redirect('https://commonknowledge.youcanbook.me')
+)
 
 module.exports = router
